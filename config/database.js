000d2e0f512ed853c5c45421f0130ec44e2fd58c
VHClarke <vanessahclarke@gmail.com>
2019-07-28 20:49:49 -0400

    savage-auth

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://VHclarke:Welcome123@cluster01-1owsi.mongodb.net/test?retryWrites=true&w=majority', // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'dbName': 'test-mongodb'
+};
